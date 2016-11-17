5ad9809165746d129f4c6a7a74cbeb8b5305569d
Bryan Lopez <lopezz.bryann966@gmail.com>
2016-11-17 11:23:49 -0800

    rara

--- simplegame.js
+++ simplegame.js
@@ -2,11 +2,11 @@ var bananaX = 250;
 document.addEventListener("keydown", function(e) {
   if(e.keyCode == 37){
     bananaX -=15;
- document.getElementById("banana").setAttribute("y", bananaX)
+ document.getElementById("banana").setAttribute("x", bananaX)
 
   }
   else if(e.keyCode == 39){
         bananaX +=15;
- document.getElementById("banana").setAttribute("y", bananaX)
+ document.getElementById("banana").setAttribute("x", bananaX)
   }
 })
