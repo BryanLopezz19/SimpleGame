76eaf87e7e86ea12e8d306a3ee1f1f2d783dae6b
Bryan Lopez <lopezz.bryann966@gmail.com>
2016-11-17 10:51:13 -0800

    n,bb

--- simplegame.js
+++ simplegame.js
@@ -1,12 +1,12 @@
 var bananaX = 250:
 document.addEventListener("keydown", function(e) {
   if(e.keyCode == 37){
-    pugX -=15;
+    bananaX -=15;
  document.getElementById("banana").setAttribute("y", bananaX)
 
   }
   else if(e.keyCode == 39){
-        pugX +=15;
+        bananaX +=15;
  document.getElementById("banana").setAttribute("y", bananaX)
   }
 })
