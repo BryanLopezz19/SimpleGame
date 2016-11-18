8ca2fafcbc765e31ba87fcfc7417edb7fb220852
Bryan Lopez <lopezz.bryann966@gmail.com>
2016-11-18 11:13:39 -0800

    up down left right

--- simplegame.js
+++ simplegame.js
@@ -10,3 +10,16 @@ document.addEventListener("keydown", function(e) {
  document.getElementById("banana").setAttribute("x", bananaX)
   }
 })
+
+var bananaY = 20;
+document.addEventListener("keydown", function(e) {
+  if(e.keyCode == 38){
+    bananaY -=15;
+ document.getElementById("banana").setAttribute("y", bananaY)
+
+  }
+  else if(e.keyCode == 40){
+        bananaY +=15;
+ document.getElementById("banana").setAttribute("y", bananaY)
+  }
+})
